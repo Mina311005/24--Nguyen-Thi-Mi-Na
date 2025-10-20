440f97abadba66a61be1d96dcc75fd207a55ad16
Mina311005 <mina050638@gmail.com>
2025-10-20 20:23:56 +0700

    Xong ham them sinh vien

---
+++ student_manager.py
@@ -0,0 +1,8 @@
+def add_student(name, year_of_birth, address):
+    student = {
+        "name": name,
+        "year_of_birth": year_of_birth,
+        "address": address
+    }
+    student_list.append(student)
+    print(f"Da them sinh vien {name} thanh cong.")
